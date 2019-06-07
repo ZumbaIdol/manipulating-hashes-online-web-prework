3dfcc9496b1dd43ab1eb8f2f8b27ed17dcbb5ea0
David Foote <david.a.foote@gmail.com>
2019-06-07 22:43:30 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          contacts.delete_if :favorite_icecream_flavors == "strawberry"
+          contacts.delete_if {|key, value| key >= "b" }
           puts "#{flavor}"
         end
       end
